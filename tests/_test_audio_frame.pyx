08f82aa3784d8b52471abd8e93ef8489886b585e
nocarryr <matt@nomadic-recording.com>
2022-11-02 13:48:15 -0500

    Update audio_frame tests

--- tests/_test_audio_frame.pyx
+++ tests/_test_audio_frame.pyx
@@ -5,10 +5,12 @@
 cimport cython
 from libc.math cimport sqrt, sin, M_PI_2
 
+import time
+
 from cyndilib.wrapper cimport *
 import numpy as np
 cimport numpy as cnp
-from cyndilib.audio_frame cimport AudioRecvFrame
+from cyndilib.audio_frame cimport AudioRecvFrame, AudioFrameSync
 
 
 
@@ -24,83 +26,77 @@ cdef void print_audio_frame_data(NDIlib_audio_frame_v3_t* p) nogil except *:
         ''')
 
 
-cdef void gen_fake_data(NDIlib_audio_frame_v3_t* frame) nogil except *:
-    cdef double fc = 2000.
-    cdef double mult = 1. / frame.sample_rate
-    cdef double amp = 2*sqrt(2)
-    cdef double offset = frame.timestamp * mult
-    cdef double t, v
-    cdef size_t i, j, k
-    cdef float* float_data = <float*>frame.p_data
-
-    for i in range(frame.no_samples):
-        t = i * mult + offset
-        v = amp * sin(M_PI_2*fc*t)
-        float_data[i] = v
-        float_data[i+frame.no_samples] = v
-
-
-@cython.boundscheck(False)
-@cython.wraparound(False)
-cdef void _test(AudioRecvFrame audio_frame, cnp.float32_t[:,:,:] samples, cnp.float32_t[:,:,:] results) nogil except *:
-    # cdef audio_bfr_p bfr_root
-    # cdef audio_bfr_p bfr = av_frame_bfr_create(bfr_root)
-    # bfr.sample_rate = 48000
-    # bfr.num_channels = 2
-    # bfr.num_samples = 6000
-
-    # cdef NDIlib_audio_frame_v3_t frame
-    cdef cnp.float32_t[:,:] sample_view
-    cdef cnp.float32_t[:,:] recv_view
-    cdef NDIlib_audio_frame_v3_t* frame = audio_frame.ptr
+def audio_frame_process_events(AudioRecvFrame audio_frame):
     cdef NDIlib_recv_instance_t recv_ptr = NULL
-    cdef size_t num_segments = samples.shape[0]
-    frame.sample_rate = 48000
-    frame.no_channels = samples.shape[1]
-    frame.no_samples = samples.shape[2]
-    frame.channel_stride_in_bytes = sizeof(float) * frame.no_samples
+
+    audio_frame._prepare_incoming(recv_ptr)
+    audio_frame._process_incoming(recv_ptr)
+
+cdef void fill_audio_frame_struct(
+    NDIlib_audio_frame_v3_t* frame,
+    cnp.float32_t[:,:] samples,
+    size_t sample_rate,
+    double timestamp,
+) except *:
+    cdef int32_t ndi_ts = posix_time_to_ndi(timestamp)
+    cdef size_t nrows = samples.shape[0], ncols = samples.shape[1]
+
+    frame.sample_rate = sample_rate
+    frame.no_channels = nrows
+    frame.no_samples = ncols
+    frame.channel_stride_in_bytes = sizeof(float) * ncols
     frame.timecode = NDIlib_send_timecode_synthesize
     frame.FourCC = NDIlib_FourCC_audio_type_FLTP
-    frame.timestamp = 0
-    # print(f'num_segments={num_segments}, frame: {frame.no_channels} ch, {frame.no_samples} samp')
-    # print('malloc')
-    # time.sleep(.1)
-    frame.p_data = <uint8_t*>mem_alloc(sizeof(float)*frame.no_channels*frame.no_samples)
-    # cdef uint8_t[sizeof(float)] tmp
+    frame.timestamp = ndi_ts
+
+    frame.p_data = <uint8_t*>mem_alloc(sizeof(float)*nrows*ncols)
     cdef float* float_data = <float*>frame.p_data
 
-    # print('gen_fake_data')
-    # time.sleep(.1)
-    cdef size_t i, j, k, l
-    for i in range(num_segments):
-        # sample_view = samples[i]
-        # print(i)
-        # time.sleep(.1)
-        l = 0
-        for j in range(frame.no_channels):
-            for k in range(frame.no_samples):
-                float_data[l] = samples[i,j,k]
-                l += 1
-        audio_frame._process_incoming(recv_ptr)
-        # print('audio_frame.len: ', audio_frame.current_frame_data.size)
-        recv_view = audio_frame#.current_frame_data
-        results[i,...] = recv_view
-        frame.timestamp += frame.no_samples
-        recv_view = None
-    # gen_fake_data(frame)
-    # print('_process_incoming')
-    # time.sleep(.1)
-    # audio_frame._process_incoming()
-    # print('process complete')
-    # time.sleep(.1)
-    # frame.timestamp += frame.no_samples
+    cdef size_t i, j, k=0
+    for i in range(nrows):
+        for j in range(ncols):
+            float_data[k] = samples[i,j]
+            k += 1
 
-    # print('mem_free')
-    # time.sleep(.1)
-    # mem_free(frame.p_data)
 
-    # print('freed')
+def fill_audio_frame(
+    AudioRecvFrame audio_frame,
+    cnp.float32_t[:,:] samples,
+    size_t sample_rate,
+    double timestamp,
+    bint do_process=True
+):
+    cdef NDIlib_audio_frame_v3_t* frame = audio_frame.ptr
+    cdef NDIlib_recv_instance_t recv_ptr = NULL
+
+    ndi_ts = fill_audio_frame_struct(frame, samples, sample_rate, timestamp)
+
+    if do_process:
+        audio_frame_process_events(audio_frame)
+
+    # print('processed')
     # time.sleep(.1)
 
-def run_test(AudioRecvFrame audio_frame, cnp.float32_t[:,:,:] samples, cnp.float32_t[:,:,:] results):
-    _test(audio_frame, samples, results)
+    cdef list indices = []
+    cdef size_t ix
+    for ix in audio_frame.read_indices:
+        indices.append(ix)
+    return frame.timestamp, indices
+
+
+def fill_audio_frame_sync(
+    AudioFrameSync audio_frame,
+    cnp.float32_t[:,:] samples,
+    size_t sample_rate,
+    double timestamp,
+    bint do_process=True
+):
+    cdef NDIlib_audio_frame_v3_t* frame = audio_frame.ptr
+    cdef NDIlib_framesync_instance_t fs_ptr = NULL
+
+    fill_audio_frame_struct(frame, samples, sample_rate, timestamp)
+
+    if do_process:
+        audio_frame._process_incoming(fs_ptr)
+
+    return frame.timestamp
--- tests/test_audio_frame.py
+++ tests/test_audio_frame.py
@@ -1,69 +1,370 @@
 import time
+from pprint import pprint
+from functools import partial
+from collections import namedtuple
+import threading
+import traceback
 import pytest
 
 import numpy as np
 
-from cyndilib.audio_frame import AudioRecvFrame
+from cyndilib.audio_frame import AudioRecvFrame, AudioFrameSync
 
-from _test_audio_frame import run_test
-
-def build_fake_data(sample_rate, num_channels, num_samples, num_segments, s_perseg):
-    # a = np.array((num_samples), dtype=np.float32)
-    # s_perseg = num_samples // num_segments
-    print(f'N={num_samples}')
-    fc = 2000
-    amp = 2*np.sqrt(2)
-    t = np.arange(num_samples) / sample_rate
-    a = amp*np.sin(2*np.pi*fc*t)
-    print(f'a.size={a.size}')
-    a = np.resize(a, (num_segments, s_perseg))
-    sp_list = [a.shape[0], a.shape[1]]
-    print(f'a.shape={sp_list}')
-    b = np.zeros((num_segments, num_channels, s_perseg), dtype=a.dtype)
-    sp_list = [b.shape[0], b.shape[1], b.shape[2]]
-    print(f'b.shape={sp_list}')
-    assert b.size == num_samples * num_channels
-    for i in range(num_channels):
-        b[:,i,:] = a
-    b = np.asarray(b, dtype=np.float32)
-    return b
+from _test_audio_frame import fill_audio_frame, fill_audio_frame_sync, audio_frame_process_events
 
 
-def test():
-    num_seconds = 64
-    num_repeats = 8
-    fs = 48000
-    N = fs * num_seconds
-    s_perseg = 6000#fs // 4
-    num_segments = N // s_perseg
+
+AudioParams = namedtuple('AudioParams', [
+    'sample_rate', 'num_channels', 'num_samples', 'num_segments', 's_perseg',
+    'samples_3d', 'samples_2d'],
+    defaults=[
+        48000, 2, 48000*2, 48000*2//6000, 6000, None, None,
+    ]
+)
+
+# DEFAULT_AUDIO_PARAMS = AudioParams(
+#     sample_rate=48000,
+#     num_channels=2,
+#     num_samples=48000 * 2,
+#     s_perseg=6000,
+#     num_segments = 48000 * 2 // 6000,
+#     samples_3d=None,
+#     samples_2d=None,
+# )
+
+@pytest.fixture
+def fake_audio_builder():
+    def build_fake_data(
+        params: AudioParams
+        # sample_rate: int,
+        # num_channels: int,
+        # num_samples: int,
+        # num_segments: int,
+        # s_perseg: int,
+    ) -> AudioParams:
+        print(f'N={params.num_samples}')
+        fc = 2000
+        sig_dbFS = -6
+        sig_amp = 10 ** (sig_dbFS/20)
+        nse_dbFS = -30
+        nse_amp = 10 ** (nse_dbFS/20)
+        t = np.arange(params.num_samples) / params.sample_rate
+        a = sig_amp*np.sin(2*np.pi*fc*t)
+        print(f'a.size={a.size}')
+        a2 = np.zeros((params.num_channels, params.num_samples), dtype=a.dtype)
+        for i in range(params.num_channels):
+            a2[i,...] = a + nse_amp*np.random.uniform(-1, 1, a.size)
+        a = np.asarray(a2, dtype=np.float32)
+        b = np.zeros((params.num_segments, params.num_channels, params.s_perseg), dtype=a.dtype)
+        s_perseg = params.s_perseg
+        for i in range(params.num_segments):
+            for j in range(params.num_channels):
+                b[i,j,:] = a[j,i*s_perseg:i*s_perseg+s_perseg]
+
+        return params._replace(samples_3d=b, samples_2d=a)
+        # result = AudioParams(
+        #     sample_rate, num_channels, num_samples, num_segments, s_perseg, b, a,
+        # )
+        # return result
+    return build_fake_data
+
+@pytest.fixture(params=[2, 8, 16, 32])
+def num_seconds(request):
+    return request.param
+
+@pytest.fixture
+def fake_audio_data(num_seconds, fake_audio_builder):
+    params = AudioParams()
+    num_samples = params.sample_rate * num_seconds
+    num_segments = num_samples // params.s_perseg
+    params = params._replace(num_samples=num_samples, num_segments=num_segments)
+    return fake_audio_builder(params)
+
+@pytest.fixture
+def fake_audio_data_longer(num_seconds, fake_audio_builder):
+    params = AudioParams()
+    num_samples = params.sample_rate * num_seconds * 2
+    num_segments = num_samples // params.s_perseg
+    params = params._replace(num_samples=num_samples, num_segments=num_segments)
+    return fake_audio_builder(params)
+
+def test_buffer_read_all(fake_audio_data):
+    fs = fake_audio_data.sample_rate
+    N = fake_audio_data.num_samples
+    num_channels = fake_audio_data.num_channels
+    max_buffers = fake_audio_data.num_segments
+    num_segments = fake_audio_data.num_segments
+    s_perseg = fake_audio_data.s_perseg
+    audio_frame = AudioRecvFrame(max_buffers=max_buffers)
+    assert audio_frame.max_buffers == max_buffers
+
+    max_bfr_samples = max_buffers * s_perseg
+    assert max_bfr_samples == N
+
+    print(f'{max_buffers=}, {max_bfr_samples=}')
+
     assert num_segments * s_perseg == N
 
-    audio_frame = AudioRecvFrame()
-    # cdef cnp.ndarray[cnp.float32_t, ndim=3] samples = build_fake_data(fs, 2, N, num_segments, s_perseg)
-    # cdef cnp.ndarray[cnp.float32_t, ndim=3] results = np.zeros((num_segments, 2, s_perseg), dtype=np.float32)
-    samples = build_fake_data(fs, 2, N, num_segments, s_perseg)
-    results = np.zeros((num_segments, 2, s_perseg), dtype=np.float32)
-    print('_test start')
-    # with nogil:
-    # cdef size_t i
-    # times = []
-    # cdef cnp.ndarray[cnp.float64_t, ndim=1] times = np.zeros(num_repeats, dtype=np.float64)
-    times = np.zeros(num_repeats, dtype=np.float64)
-    g_start = time.time()
-    # cdef double start_ts, end_ts, duration
-    for i in range(num_repeats):
-        start_ts = time.time()
-        run_test(audio_frame, samples, results)
-        end_ts = time.time()
-        duration = end_ts - start_ts
-        times[i] = duration
-        assert np.array_equal(samples, results)
-    g_end = time.time()
-    print('_test return')
-    times /= 1000
-    min_t = times.min()
-    max_t = times.max()
-    avg_t = times.mean()
-    print(f'min: {min_t}ms, max: {max_t}, avg: {avg_t}')
-    time.sleep(.1)
-    # np.savez('audio_frame_test', samples=samples, results=results)
+
+    samples = fake_audio_data.samples_3d
+    samples_flat = fake_audio_data.samples_2d
+    timestamps = np.arange(num_segments) / fs * s_perseg
+    ndi_timestamps = np.zeros(num_segments, dtype=np.int32)
+    results = np.zeros((num_segments, num_channels, s_perseg), dtype=np.float32)
+    # read_timestamps = np.zeros_like(ndi_timestamps)
+
+    for i in range(max_buffers):
+        ndi_ts, read_indices = fill_audio_frame(audio_frame, samples[i], fs, timestamps[i])
+        ndi_timestamps[i] = ndi_ts
+        bfr_depth = audio_frame.get_buffer_depth()
+        # print(f'{read_indices=}')
+        print(f'{i=} {bfr_depth=}')
+        assert bfr_depth == i + 1
+    # print('buffering complete')
+    # time.sleep(.2)
+
+    num_read_samples = audio_frame.get_read_length()
+    assert num_read_samples == max_bfr_samples
+    # print('get_all_read_data()')
+    # time.sleep(.1)
+    read_data, read_timestamps = audio_frame.get_all_read_data()
+    # time.sleep(.1)
+    assert audio_frame.get_read_length() == 0
+    print(f'{read_data.shape=}')
+
+    assert read_data.shape[1] == num_read_samples
+    num_read_segments = num_read_samples // s_perseg
+    print(f'{num_segments=}, {num_read_segments=}, {num_read_samples=}')
+    assert np.array_equal(samples_flat, read_data)
+    assert np.array_equal(ndi_timestamps, read_timestamps)
+    col_idx = 0
+    for i in range(num_read_segments):
+        print(f'{i=}, {col_idx=}')
+        results[i,...] = read_data[:, col_idx:col_idx+s_perseg]
+        col_idx += s_perseg
+    assert np.array_equal(samples[:max_buffers,...], results[:max_buffers,...])
+    # print('exit')
+    # time.sleep(.5)
+
+
+def test_buffer_read_single(fake_audio_data_longer):
+    fake_audio_data = fake_audio_data_longer
+    fs = fake_audio_data.sample_rate
+    N = fake_audio_data.num_samples
+    num_channels = fake_audio_data.num_channels
+    max_buffers = fake_audio_data.num_segments // 2
+    num_segments = fake_audio_data.num_segments
+    s_perseg = fake_audio_data.s_perseg
+    audio_frame = AudioRecvFrame(max_buffers=max_buffers)
+
+    assert max_buffers * s_perseg == N // 2
+
+    samples = fake_audio_data.samples_3d
+    samples_flat = fake_audio_data.samples_2d
+    timestamps = np.arange(num_segments) / fs * s_perseg
+    ndi_timestamps = np.zeros(num_segments, dtype=np.int32)
+    results = np.zeros((num_segments, num_channels, s_perseg), dtype=np.float32)
+
+
+    for i in range(num_segments):
+        # print(f'{i=}, {audio_frame.view_count=}')
+        ndi_ts, read_indices = fill_audio_frame(audio_frame, samples[i], fs, timestamps[i])
+        ndi_timestamps[i] = ndi_ts
+        print(f'{read_indices=}')
+        assert audio_frame.get_buffer_depth() == 1
+        read_data, read_timestamp = audio_frame.get_read_data()
+        assert read_data.shape == (num_channels, s_perseg)
+        results[i,...] = read_data
+        assert read_timestamp == ndi_timestamps[i]
+        assert audio_frame.get_buffer_depth() == 0
+    assert np.array_equal(samples, results)
+
+def test_buffer_fill_read_data(fake_audio_data):
+    fs = fake_audio_data.sample_rate
+    N = fake_audio_data.num_samples
+    num_channels = fake_audio_data.num_channels
+    max_buffers = fake_audio_data.num_segments
+    num_segments = fake_audio_data.num_segments
+    s_perseg = fake_audio_data.s_perseg
+    audio_frame = AudioRecvFrame(max_buffers=max_buffers)
+
+    samples = fake_audio_data.samples_3d
+    samples_flat = fake_audio_data.samples_2d
+    timestamps = np.arange(num_segments) / fs * s_perseg
+    ndi_timestamps = np.zeros(num_segments, dtype=np.int32)
+    results = np.zeros((num_segments, num_channels, s_perseg), dtype=np.float32)
+    read_data = np.zeros((num_channels, s_perseg), dtype=np.float32)
+
+    for i in range(num_segments):
+        ndi_ts, read_indices = fill_audio_frame(audio_frame, samples[i], fs, timestamps[i])
+        ndi_timestamps[i] = ndi_ts
+        read_timestamp = audio_frame.fill_read_data(read_data)
+        assert read_timestamp == ndi_ts
+        assert np.array_equal(read_data, samples[i])
+        results[i,...] = read_data[...]
+
+    assert np.array_equal(samples, results)
+
+def test_buffer_fill_read_data_threaded(fake_audio_data, listener_pair):
+    fs = fake_audio_data.sample_rate
+    N = fake_audio_data.num_samples
+    num_channels = fake_audio_data.num_channels
+    max_buffers = fake_audio_data.num_segments
+    num_segments = fake_audio_data.num_segments
+    s_perseg = fake_audio_data.s_perseg
+    audio_frame = AudioRecvFrame(max_buffers=max_buffers)
+
+    samples = fake_audio_data.samples_3d
+    samples_flat = fake_audio_data.samples_2d
+    timestamps = np.arange(num_segments) / fs * s_perseg
+    ndi_timestamps = np.zeros(num_segments, dtype=np.int32)
+    results = np.zeros((num_segments, num_channels, s_perseg), dtype=np.float32)
+    result_timestamps = np.zeros(num_segments, dtype=np.int64)
+    read_data = np.zeros((num_channels, s_perseg), dtype=np.float32)
+    read_timestamps = np.zeros(num_segments, dtype=np.int64)
+
+
+    sender_thread, listener_thread = listener_pair
+    sender_thread.audio_frame = audio_frame
+    # sender_thread.callback = process_callback
+    sender_thread.run_forever = True
+
+    i = 0
+    read_idx, write_idx = 0, 0
+    num_written, num_read = 0, 0
+    num_remain = num_segments
+    while write_idx < num_segments:
+        print(f'{i=}, {num_written=}, {num_read=}, {num_remain=}')
+        print(f'{read_idx=}, {write_idx=}')
+        ndi_ts, read_indices = fill_audio_frame(
+            audio_frame, samples[write_idx], fs, timestamps[write_idx], do_process=False
+        )
+        print(read_indices)
+        ndi_timestamps[write_idx] = ndi_ts
+        for state, data in listener_thread:
+            print(f'{state=}, {data=}')
+            if state == 'process_triggered':
+                pass
+            elif state == 'process_finished':
+                num_written += 1
+                num_remain -= 1
+                proc_bfr_len, proc_ts = data
+                assert proc_bfr_len == num_written - num_read == audio_frame.get_buffer_depth()
+                assert proc_ts == ndi_timestamps[write_idx]
+
+                write_idx += 1
+                next_ndi_ts, read_indices = fill_audio_frame(
+                    audio_frame, samples[write_idx], fs, timestamps[write_idx], do_process=True
+                )
+                print(read_indices)
+                ndi_timestamps[write_idx] = next_ndi_ts
+                assert audio_frame.get_buffer_depth() == proc_bfr_len + 1
+                # assert next_ndi_ts ==
+                num_written += 1
+                num_remain -= 1
+                assert audio_frame.get_buffer_depth() == num_written - num_read
+                read_timestamp = audio_frame.fill_read_data(read_data)
+                assert read_timestamp == ndi_timestamps[read_idx]
+                assert np.array_equal(read_data, samples[read_idx])
+                write_idx += 1
+                read_idx += 1
+                num_read += 1
+                # num_remain -= 1
+            elif state == 'callback_result':
+                pass
+        i += 2
+
+
+def test_buffer_overwrite(fake_audio_data_longer):
+    fake_audio_data = fake_audio_data_longer
+    fs = fake_audio_data.sample_rate
+    N = fake_audio_data.num_samples
+    num_channels = fake_audio_data.num_channels
+    max_buffers = fake_audio_data.num_segments // 2
+    num_segments = fake_audio_data.num_segments
+    s_perseg = fake_audio_data.s_perseg
+    audio_frame = AudioRecvFrame(max_buffers=max_buffers)
+
+    assert max_buffers * s_perseg == N // 2
+
+    samples = fake_audio_data.samples_3d
+    samples_flat = fake_audio_data.samples_2d
+    timestamps = np.arange(num_segments) / fs * s_perseg
+    ndi_timestamps = np.zeros(num_segments, dtype=np.int32)
+    results = np.zeros((num_segments, num_channels, s_perseg), dtype=np.float32)
+
+    for i in range(num_segments):
+        ndi_ts, read_indices = fill_audio_frame(audio_frame, samples[i], fs, timestamps[i])
+        ndi_timestamps[i] = ndi_ts
+        bfr_len = audio_frame.get_buffer_depth()
+        print(f'{i=}, {bfr_len=}, {read_indices=}')
+        cur_timestamp = audio_frame.get_timestamp()
+        assert cur_timestamp == ndi_ts
+
+        if i >= max_buffers:
+            assert bfr_len == max_buffers
+            segment_index = i - max_buffers + 1
+            assert segment_index > 0
+            print(f'{segment_index=}, {cur_timestamp=}')
+            read_data, read_timestamp = audio_frame.get_read_data()
+            assert np.array_equal(read_data, samples[segment_index,...])
+            assert read_timestamp == ndi_timestamps[segment_index]
+
+
+
+def test_frame_sync(fake_audio_data_longer):
+    fake_audio_data = fake_audio_data_longer
+    # fs = 48000
+    # N = 60
+    # num_channels = 2
+
+    fs = fake_audio_data.sample_rate
+    N = fake_audio_data.num_samples
+    num_channels = fake_audio_data.num_channels
+    max_buffers = fake_audio_data.num_segments // 2
+    num_segments = fake_audio_data.num_segments
+    s_perseg = fake_audio_data.s_perseg
+    audio_frame = AudioFrameSync()
+
+    def iter_divisors():
+        while True:
+            yield from range(2, 32)
+    divisor_iter = iter_divisors()
+
+    def next_sample_length(num_samples_used):
+        num_remain = N - num_samples_used
+        if num_remain < 3:
+            return num_remain
+        i = 0
+        while True:
+            divisor = next(divisor_iter)
+            if num_remain % divisor == 0:
+                return num_remain // divisor
+            i += 1
+            if i > 32:
+                return num_remain
+
+    samples = fake_audio_data.samples_3d
+    samples_flat = fake_audio_data.samples_2d
+    ndi_timestamps = []
+    results = []
+
+    num_samples_used = 0
+    s_idx = 0
+    e_idx = 0
+    last_ts = 0
+    while num_samples_used < N:
+        samp_len = next_sample_length(num_samples_used)
+        # print(f'{samp_len=}, {num_samples_used=}')
+        e_idx = s_idx + samp_len
+        src_samples = samples_flat[:,s_idx:e_idx]
+        fill_audio_frame_sync(audio_frame, src_samples, fs, last_ts)
+        r = audio_frame.get_array()
+        assert np.array_equal(src_samples, r)
+        results.append(r)
+        last_ts += samp_len / fs
+        num_samples_used += samp_len
+        s_idx = e_idx
+
+    results = np.concatenate(results, axis=1)
+    assert np.array_equal(samples_flat, results)
