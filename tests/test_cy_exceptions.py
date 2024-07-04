531fcf5663df809b86ad52d8f8ece789a9006bad
nocarryr <matt@nomadic-recording.com>
2024-07-04 13:52:55 -0500

    Add tests to verify assumtions in 2003894

---
+++ tests/test_cy_exceptions.py
@@ -0,0 +1,22 @@
+import pytest
+
+from _test_exceptions import raise_py_exc
+
+@pytest.fixture(params=[
+    Exception,
+    RuntimeError,
+    KeyError,
+    IndexError,
+    ValueError,
+    TypeError,
+    TypeError,
+    MemoryError,
+    ZeroDivisionError,
+])
+def exception_type(request):
+    return request.param
+
+def test_cy_exceptions(exception_type):
+    msg = 'foobar!'
+    with pytest.raises(exception_type, match=msg):
+        raise_py_exc(exception_type, msg)
