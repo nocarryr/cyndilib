977c5cfc18e9438dbce862bb3ff343b38de30826
nocarryr <matt@nomadic-recording.com>
2024-07-13 16:07:07 -0500

    Revert ba828e0

--- src/cyndilib/__init__.py
+++ src/cyndilib/__init__.py
@@ -1,17 +1,14 @@
 import os
 import sys
-if sys.version_info < (3, 9):
-    import importlib_resources
-else:
-    import importlib.resources as importlib_resources
+from pathlib import Path
+from pkg_resources import resource_filename
 
 if sys.platform == 'win32':
-    dll_dir = importlib_resources.files('cyndilib.wrapper.bin')
-    os.add_dll_directory(str(dll_dir))
+    dll_dir = Path(resource_filename(__name__, '')) / 'wrapper' / 'bin'
+    os.add_dll_directory(dll_dir)
 
 def get_include() -> str:
-    p = importlib_resources.files('cyndilib.wrapper.include')
-    return str(p)
+    return resource_filename('cyndilib.wrapper.include', '')
 
 
 from .wrapper import *
